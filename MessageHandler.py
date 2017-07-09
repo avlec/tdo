bd5d5b79133418158b8d9c1bcd291609497d62d0
avlec <daqx1444@gmail.com>
2017-07-08 23:42:03 -0700

    updated MessageHandler framework, added a unit test template for MessageHandler

--- MessageHandler.py
+++ MessageHandler.py
@@ -1,19 +1,53 @@
+from Queue import *
 
+
+# TODO design and implement
 class MessageHandler:
+
+    message_sender = None
+    message_receiver = None
+    message_processor = None
+
     def __init__(self):
+        message_sender = MessageSender()
+        message_receiver = MessageReceiver()
+        message_processor = MessageProcessor()
+
+    def run(self):
         pass
 
 
+# TODO design and implement
 class MessageProcessor:
+
+    process_queue = None
+
     def __init__(self):
         pass
 
+    def run(self):
+        pass
 
+
+# TODO design and implement
 class MessageSender:
+
+    outbound_queue = None
+
     def __init__(self):
         pass
 
+    def run(self):
+        pass
 
+
+# TODO design and implement
 class MessageReceiver:
+
+    inbound_queue = None
+
     def __init__(self):
+        pass
+
+    def run(self):
         pass
