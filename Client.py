1024a0b2d3a828858ceb66aa4393fe5fdf658a20
Michail Roesli <misha.roesli@gmail.com>
2017-07-12 21:08:33 -0700

    Added import and commented
    Imported "select" library and commented out code I added earlier.

--- Client.py
+++ Client.py
@@ -2,7 +2,7 @@ import random
 import threading
 import socket
 import sys
-
+import select
 
 # format: message id|message sender id|message channel id|message
 # ids are 10 digits, message is limited to 256 characters
@@ -13,6 +13,10 @@ class Client:
     def __init__(self):
         self.id = None
         self.currentChannel = None
+
+    # def chat_client(self):
+    #
+    #
 
 
 if __name__ == "__main__":
