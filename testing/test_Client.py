7773b7db71e260b04c4a019f1e10da388da4f9e3
Michail Roesli <misha.roesli@gmail.com>
2017-07-27 15:51:38 -0700

    Added test_Client.py

---
+++ testing/test_Client.py
@@ -0,0 +1,15 @@
+#from MessageHandler import *
+import unittest
+
+
+# TODO add test cases for commands
+class TestMessageHandlerMethods(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_creation(self):
+        pass
