da668c7036ff3cad178ed8f8010f21ea9d2db16d
Michail Roesli <misha.roesli@gmail.com>
2017-08-01 21:47:44 -0700

    test_client initialized

--- testing/test_Client.py
+++ testing/test_Client.py
@@ -7,6 +7,7 @@ class TestMessageHandlerMethods(unittest.TestCase):
 
     def test_client_command_1(self):
         pass
+        #server = Server()
 
     def test_client_command_2(self):
         pass
